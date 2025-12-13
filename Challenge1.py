19b1d5d5a49c1940c9240f616ccd6cfa5203ac1f
supritha-rendedla <supritharendedla@gmail.com>
2025-12-13 11:49:54 +0530

    Add files via upload

---
+++ Challenge1.py
@@ -0,0 +1,7 @@
+#Python Challenge
+# Use PRINT() to recreate the exact output
+# you are allowed to use only onr print()
+print("""Your Learning Path:
+\t- Python Basics
+\t- Data Engineering
+\t- AI""")
