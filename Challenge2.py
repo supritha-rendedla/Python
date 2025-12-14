186a68f43b197fc2891783f9f5d3deef8361971b
Supritha Rendedla <supritharendedla@gmail.com>
2025-12-14 16:08:15 +0530

    Add files via upload

---
+++ Challenge2.py
@@ -0,0 +1,9 @@
+"""Print the following three lines
+add a vatiable to make it dynamic
+info@datawithawithbaraa.com
+support@datawithbaraa.com
+www.datawithbaraa.com"""
+a="datawithbaraa.com"
+print("info@"+a)
+print("support@"+a)
+print("www."+a)
---
+++ Variable.py
@@ -0,0 +1,8 @@
+"""print("Supritha")
+print("Supritha is learning Python")
+print("Supritha Wants to become a python expert")"""
+name="Supritha"
+language="Python"
+print(name)
+print(name,"is learning",language)
+print(name,"wants to become a",language,"expert")
