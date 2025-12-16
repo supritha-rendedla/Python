2c7d440f73c7dc5c7a840d8289ade82ea09aad12
Supritha Rendedla <supritharendedla@gmail.com>
2025-12-16 21:29:17 +0530

    Add files via upload

---
+++ Challenge3.py
@@ -0,0 +1,29 @@
+"""create 5 variables - each with a different data type:
+1. age
+2. Height
+3. name
+4. are you a student?
+5. Something with no value yet"""
+age=20
+height=154.5
+name="Supritha"
+stu=True
+salary=""
+print(age,height,name,stu,salary)
+#20 154.5 Supritha True 
+print(type(age))
+print(type(height))
+print(type(name))
+print(type(stu))
+print(type(salary))
+"""<class 'int'>
+<class 'float'>
+<class 'str'>
+<class 'bool'>
+<class 'str'>"""
+print(len(age))
+print(len(height))
+print(len(name))
+print(len(stu))
+print(len(salary))
+
