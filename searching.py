1639a5f7ae920ccf2821b6b27f09f23fd921d430
Supritha Rendedla <supritharendedla@gmail.com>
2025-12-30 13:37:27 +0530

    Add files via upload

---
+++ searching.py
@@ -0,0 +1,14 @@
+num1="+91-789-359-2079"
+#startswith()
+print(num1.startswith("+91"))
+mail="supritharendedla@gmail.com"
+#endswith()
+print(mail.endswith("@gmail.com"))
+#in
+print('supritha'in mail)
+num2="+1-789-359-2079"
+num3="+878-789-359-2079"
+#find()
+print(num1[num1.find("-")+1:])
+print(num2[num2.find("-")+1:])
+print(num3[num3.find("-")+1:])
